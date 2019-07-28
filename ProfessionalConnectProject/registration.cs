eb44d21f47cd3958008f7e05375bf16d82311a8f
Vandana Ramaprasad <vandanar1612@gmail.com>
2019-07-27 21:47:45 -0400

    database and registration cs created

--- ProfessionalConnectProject/registration.cs
+++ ProfessionalConnectProject/registration.cs
@@ -15,6 +15,8 @@ namespace ProfessionalConnectProject
     [Activity(Label = "registration")]
     public class registration : Activity
     {
+
+       // EditText myfname, mylname, myuserid, mypass, myphone, myrole;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -22,6 +24,34 @@ namespace ProfessionalConnectProject
             // Create your application here
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.registrationView);
+
+            userDBHelper myuserDBHelper;
+
+            myuserDBHelper = new userDBHelper(this);
+
+    
+
+         // inserting values to the reg table 
+            myuserDBHelper.insertValue("vandana","R","v@mail.com","1234",0344,"BA");
+            myuserDBHelper.insertValue("Sahil", "M", "S@mail.com", "12345",0455, "TL");
+            myuserDBHelper.insertValue("Sandarbh", "MS", "MS@mail.com", "123456",0211, "SE");
+
+            myuserDBHelper.selectMyValuesReg();
+
+    
+         // inserting values to the student table 
+            myuserDBHelper.insertValue("v@mail.com", "HTML,CSS,JS", "B.E", "4", "PEGA");
+            myuserDBHelper.insertValue("S@mail.com", "JS,JQuery,Angular", "M.S", "5", "ORACLE");
+            myuserDBHelper.insertValue("MS@mail.com", "Android,C#,JS", "P.G", "3", "SPLUNK");
+
+            myuserDBHelper.selectMyValuesStu();
+
+         // inserting values into the emp table
+            myuserDBHelper.insertValue("v@mail.com", "ABC","HR");
+            myuserDBHelper.insertValue("S@mail.com", "XYZ","IT");
+            myuserDBHelper.insertValue("MS@mail.com", "XXX","CEO");
+
+            myuserDBHelper.selectMyValuesEmp();
 
         }
     }
--- ProfessionalConnectProject/userDBHelper.cs
+++ ProfessionalConnectProject/userDBHelper.cs
@@ -5,6 +5,8 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Database;
+using Android.Database.Sqlite;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -12,7 +14,271 @@ using Android.Widget;
 
 namespace ProfessionalConnectProject
 {
-    class userDBHelper
+    class userDBHelper : SQLiteOpenHelper
     {
+        private static string _DatabaseName = "myuserdatabase.db";
+
+     // Table #1 - Reg table Common Table for both user and emp
+        private const string TableName1 = "REG_TABLE";                
+        private const string ColumnFname = "FIRST_NAME";
+        private const string ColumnLname = "LAST_NAME";
+        private const string ColumnUserId = "ID";
+        private const string ColumnPassowrd = "PASS";
+        private const string ColumnPhone = "PHONE";
+        // private const string ColumnRePass = "repass";
+        private const string ColumnRole = "ROLE";
+
+
+     // Reg Table creation Query
+     
+        public const string CreateRegTable = "CREATE TABLE " +
+            TableName1 + " ( " + ColumnFname + " TEXT, "
+              + ColumnLname + " TEXT, "
+              + ColumnUserId + " TEXT, "
+              + ColumnPassowrd + " TEXT, "
+              + ColumnPhone + " INTEGER, "
+              //  + ColumnRePass + "TEXT,"
+              + ColumnRole + " TEXT, " +
+            "PRIMARY KEY(" + ColumnUserId + "));";
+
+
+    // Table #2 for user or student
+
+        private const string TableName2 = "STUDENT_TABLE";
+        private const string ColumnStuId = "STU_ID";
+        private const string ColumnSkills = "SKILLS";
+        private const string ColumnEdu = "EDUCATION";
+        private const string ColumnExp = "EXPERIENCE";
+        private const string ColumnCert = "CERTIFICATION";
+
+     // Student table creation Query
+        public const string CreateStudentTable = "CREATE TABLE " +
+            TableName2 + " ( " + ColumnStuId + " TEXT, "
+            + ColumnSkills + " TEXT, "
+            + ColumnEdu + " TEXT, "
+            + ColumnExp + " TEXT, "
+            + ColumnCert + " TEXT, " +
+            "FOREIGN KEY ( " + ColumnStuId + " ) " + "REFERENCES " +TableName1 + " ( " +ColumnUserId +" ));" ;
+
+
+     // Table #3 for  Employer
+
+        private const string TableName3 = "EMP_TABLE";
+        private const string ColumnEmpId = "ID";
+        private const string ColumnCmp = "COMPANY";
+        private const string ColumnPos = "POSITION";
+
+     // Emp table creation Query   
+
+        public const string CreateEmpTable = "CREATE TABLE " +
+            TableName3 + " ( " + ColumnEmpId + " TEXT, "
+            + ColumnCmp + " TEXT, "
+            + ColumnPos + " TEXT, "
+            + "FOREIGN KEY( " + ColumnEmpId + " ) " + "REFERENCES " +TableName1 + " ( " + ColumnUserId +"));" ;
+
+
+        SQLiteDatabase myDBuser;    // user database
+        Context myContext;
+        public userDBHelper(Context context) : base(context, name: _DatabaseName, factory: null, version: 1)
+        {
+            myContext = context;
+            myDBuser = WritableDatabase;
+        }
+
+
+
+        public override void OnCreate(SQLiteDatabase db)
+        {
+            db.ExecSQL(CreateRegTable);
+            db.ExecSQL(CreateStudentTable);
+            db.ExecSQL(CreateEmpTable);
+        }
+
+
+         // insert values for reg table 
+        public void insertValue(string fnameValue, string lnameValue, string emailValue, string passValue,int phone, string myroleValue)
+        {
+            // insert values into the user DB
+
+            String insertSQLReg = "insert into  " + TableName1 +" ( "+ 
+                                                    ColumnFname + ", "+ 
+                                                    ColumnLname + ", " + 
+                                                    ColumnUserId + ", " + 
+                                                    ColumnPassowrd + ", " + 
+                                                    ColumnPhone + ", " + 
+                                                    ColumnRole + ") " +
+                " values ( '" + fnameValue + "', '" + 
+                               lnameValue + "', '" + 
+                               emailValue + "', '" + 
+                               passValue + "', " + 
+                               phone + ", '" + 
+                               myroleValue + "');" ;
+
+            System.Console.WriteLine("Insert SQL " + insertSQLReg);
+            myDBuser.ExecSQL(insertSQLReg);
+        }
+
+        // insert values for student table
+
+        public void insertValue(string stuid, string skills, string education, string experience, string certification)
+        {
+            String insertSQLStu = "insert into "  + TableName2 + " ( " +
+                                                   ColumnStuId + ", " +
+                                                   ColumnSkills + ", " +
+                                                   ColumnEdu + ", " +
+                                                   ColumnExp + ", " +
+                                                   ColumnCert + ") " +
+
+            " values ( '" + stuid + "', '" +
+                            skills + "', '" +
+                            education + "', '" +
+                            experience + "', '" +
+                            certification + "');" ;
+
+            System.Console.WriteLine("Insert SQL " + insertSQLStu);
+            myDBuser.ExecSQL(insertSQLStu);
+        }
+
+        // insert values for emp table
+
+        public void insertValue(string empid, string company, string position)
+        {
+            String insertSQLEmp = "insert into " + TableName3 + " ( " +
+                                                   ColumnEmpId + ", " +
+                                                   ColumnCmp + ", " +
+                                                   ColumnPos + ") " +
+
+            " values ( '" + empid + "', '" +
+                            company + "', '" +
+                            position + "');";
+
+            System.Console.WriteLine("Insert SQL " + insertSQLEmp);
+            myDBuser.ExecSQL(insertSQLEmp);
+        }
+
+
+        // display values for reg table
+        public void selectMyValuesReg()
+        {
+
+            String sqlQuery = "Select * from " + TableName1;
+
+            ICursor result = myDBuser.RawQuery(sqlQuery, null);
+
+            while (result.MoveToNext())
+            {
+
+                
+
+                var FNamefromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnFname));
+                System.Console.WriteLine(" Value  Of FirstName  FROM DB  --> " + FNamefromDB);
+
+                var LNamefromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnLname));
+                System.Console.WriteLine(" Value  Of LastName  FROM DB  --> " + LNamefromDB);
+
+                var IDfromDB = result.GetInt(result.GetColumnIndexOrThrow(ColumnUserId));
+                System.Console.WriteLine(" Value Of ID FROM DB --> " + IDfromDB);
+
+                var PassfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnPassowrd));
+                System.Console.WriteLine(" Value  Of Password  FROM DB --> " + PassfromDB);
+
+                var RolefromDB = result.GetInt(result.GetColumnIndexOrThrow(ColumnRole));
+                System.Console.WriteLine(" Value Of Role FROM DB --> " + RolefromDB);
+
+            }
+
+
+        }
+
+        // display values for Student table 
+
+        public void selectMyValuesStu()
+        {
+
+            String sqlQuery = "Select * from " + TableName2;
+
+            ICursor result = myDBuser.RawQuery(sqlQuery, null);
+
+            while (result.MoveToNext())
+            {
+
+
+
+                var StuIDfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnStuId));
+                System.Console.WriteLine(" Value  Of StuID  FROM DB  --> " + StuIDfromDB);
+
+                var SkillsfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnSkills));
+                System.Console.WriteLine(" Value  Of Skills  FROM DB  --> " + SkillsfromDB);
+
+                var EdufromDB = result.GetInt(result.GetColumnIndexOrThrow(ColumnEdu));
+                System.Console.WriteLine(" Value Of Edu FROM DB --> " + EdufromDB);
+
+                var ExpfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnExp));
+                System.Console.WriteLine(" Value  Of Exp  FROM DB --> " + ExpfromDB);
+
+                var CertfromDB = result.GetInt(result.GetColumnIndexOrThrow(ColumnCert));
+                System.Console.WriteLine(" Value Of Cert FROM DB --> " + CertfromDB);
+
+            }
+
+
+        }
+
+        
+
+        // display values for Employee table 
+
+        public void selectMyValuesEmp()
+        {
+
+            String sqlQuery = "Select * from " + TableName3;
+
+            ICursor result = myDBuser.RawQuery(sqlQuery, null);
+
+            while (result.MoveToNext())
+            {
+
+                var EmpIDfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnEmpId));
+                System.Console.WriteLine(" Value  Of EmpID  FROM DB  --> " + EmpIDfromDB);
+
+                var CmpfromDB = result.GetString(result.GetColumnIndexOrThrow(ColumnCmp));
+                System.Console.WriteLine(" Value  Of Company  FROM DB  --> " + CmpfromDB);
+
+                var PosfromDB = result.GetInt(result.GetColumnIndexOrThrow(ColumnPos));
+                System.Console.WriteLine(" Value Of Pos FROM DB --> " + PosfromDB);
+
+            }
+
+
+        }
+
+
+        /*
+        public ICursor CheckIfEmailExist(String email, String password)
+        {
+
+            String sqlQuery = "Select * from " + TableName1 + " Where " + ColumnUserId + " = '" + email + "' AND " + ColumnPassowrd + " = '" + password + "'";
+
+            ICursor result = myDBuser.RawQuery(sqlQuery, null);
+
+            if (result.Count > 0)
+            {
+
+                System.Console.WriteLine(" Email found ");
+                return result;
+
+            }
+            else
+            {
+                System.Console.WriteLine(" Email not found ");
+                return result;
+            }
+        }
+
+    */
+        public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
