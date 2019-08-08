252937c84b33cb2f40f0e98841a831ba8d715a78
sahilmanjiyani <sahil.manjiyani14@gmail.com>
2019-08-08 04:52:05 -0400

    fix

--- ProfessionalConnectProject/Resources/Resource.designer.cs
+++ ProfessionalConnectProject/Resources/Resource.designer.cs
@@ -3123,26 +3123,26 @@ namespace ProfessionalConnectProject
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020077
-			public const int avd_hide_password_1 = 2130837623;
+			// aapt resource value: 0x7f020074
+			public const int avd_hide_password_1 = 2130837620;
 			
-			// aapt resource value: 0x7f020078
-			public const int avd_hide_password_2 = 2130837624;
+			// aapt resource value: 0x7f020075
+			public const int avd_hide_password_2 = 2130837621;
 			
-			// aapt resource value: 0x7f020079
-			public const int avd_hide_password_3 = 2130837625;
+			// aapt resource value: 0x7f020076
+			public const int avd_hide_password_3 = 2130837622;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f02007a
-			public const int avd_show_password_1 = 2130837626;
+			// aapt resource value: 0x7f020077
+			public const int avd_show_password_1 = 2130837623;
 			
-			// aapt resource value: 0x7f02007b
-			public const int avd_show_password_2 = 2130837627;
+			// aapt resource value: 0x7f020078
+			public const int avd_show_password_2 = 2130837624;
 			
-			// aapt resource value: 0x7f02007c
-			public const int avd_show_password_3 = 2130837628;
+			// aapt resource value: 0x7f020079
+			public const int avd_show_password_3 = 2130837625;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -3210,11 +3210,11 @@ namespace ProfessionalConnectProject
 			// aapt resource value: 0x7f02006b
 			public const int notification_icon_background = 2130837611;
 			
-			// aapt resource value: 0x7f020075
-			public const int notification_template_icon_bg = 2130837621;
+			// aapt resource value: 0x7f020072
+			public const int notification_template_icon_bg = 2130837618;
 			
-			// aapt resource value: 0x7f020076
-			public const int notification_template_icon_low_bg = 2130837622;
+			// aapt resource value: 0x7f020073
+			public const int notification_template_icon_low_bg = 2130837619;
 			
 			// aapt resource value: 0x7f02006c
 			public const int notification_tile_bg = 2130837612;
@@ -3223,25 +3223,16 @@ namespace ProfessionalConnectProject
 			public const int notify_panel_notification_icon_bg = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int Sahil = 2130837614;
+			public const int starIcon = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int sandu = 2130837615;
+			public const int tooltip_frame_dark = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int starIcon = 2130837616;
+			public const int tooltip_frame_light = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int tooltip_frame_dark = 2130837617;
-			
-			// aapt resource value: 0x7f020072
-			public const int tooltip_frame_light = 2130837618;
-			
-			// aapt resource value: 0x7f020073
-			public const int userIcon = 2130837619;
-			
-			// aapt resource value: 0x7f020074
-			public const int vandu = 2130837620;
+			public const int userIcon = 2130837617;
 			
 			static Drawable()
 			{
